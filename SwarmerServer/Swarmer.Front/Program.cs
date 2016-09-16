7517f718df0ad9b6e0c764c172bbfd8e464588f7
VolhvPorechja <VolhvPorechja>
2016-09-16 22:18:19 +0300

    * Initial project for frontend

---
+++ SwarmerServer/Swarmer.Front/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Swarmer.Front
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseIISIntegration()
+                .UseUrls("http://localhost:5002/")
+				.UseStartup<Startup>()
+				.Build();
+
+			host.Run();
+		}
+	}
+}
